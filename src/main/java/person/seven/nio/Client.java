0657db36ec71dc997a8612158ec4d9a1934d18fb
liuqi <liuqi>
2018-05-15 11:41:15 +0800

    cyclicBarrier

--- src/main/java/person/seven/nio/Client.java
+++ src/main/java/person/seven/nio/Client.java
@@ -11,25 +11,20 @@ public class Client {
     public static void main(String[] args) {
 
         //创建连接的地址
-        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8765);
-
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 1024);
         //声明连接通道
         SocketChannel sc = null;
-
         //建立缓冲区
         ByteBuffer buf = ByteBuffer.allocate(1024);
-
         try {
             //打开通道
             sc = SocketChannel.open();
             //进行连接
             sc.connect(address);
-
             while (true) {
                 //定义一个字节数组，然后使用系统录入功能：
                 byte[] bytes = new byte[1024];
                 System.in.read(bytes);
-
                 //把数据放到缓冲区中
                 buf.put(bytes);
                 //对缓冲区进行复位
