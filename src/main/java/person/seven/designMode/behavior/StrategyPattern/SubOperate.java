090d9e6001fcb29ca2470520687a767e86fcf80a
liuqi <liuqi>
2018-08-16 10:47:41 +0800

    策略模式

---
+++ src/main/java/person/seven/designMode/behavior/StrategyPattern/SubOperate.java
@@ -0,0 +1,1 @@
+package person.seven.designMode.behavior.StrategyPattern;/** * @description: * @author: liuqi create on 2018/8/16 **/public class SubOperate implements Strategy {    @Override    public int doOperate(int number1, int number2) {        return number1-number2;    }}
